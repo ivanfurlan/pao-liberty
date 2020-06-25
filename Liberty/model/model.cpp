d43d6eb69dcdd7de5d0502a85f67f9f2bb18fa18
Ivan Furlan <ivanfurlan98@gmail.com>
2020-06-26 01:20:26 +0200

    Aggiunto model

--- Liberty/model/model.cpp
+++ Liberty/model/model.cpp
@@ -1,6 +1,44 @@
 #include "model.h"
-
 Model::Model()
 {
 
+}
+
+Model::~Model()
+{
+    for(auto it=veicoli.begin();it<veicoli.end();++it){
+        delete *it;
+    }
+}
+
+const Veicolo* Model::getVeicoloAt(const u_int & i) const
+{
+    return veicoli[i];
+}
+
+u_int Model::count() const
+{
+    return veicoli.countElements();
+}
+
+void Model::add(Veicolo * v)
+{
+    veicoli.insertBack(v);
+}
+
+void Model::rifornisci(Veicolo * v, Rifornimento * r)
+{
+    v->addRifornimento(r);
+}
+
+void Model::delelte(Container<Veicolo*>::iterator & it)
+{
+    veicoli.remove(it);
+}
+
+void Model::deleteAll()
+{
+    while(veicoli.end()==veicoli.begin()){
+        veicoli.removeBack();
+    }
 }
--- Liberty/model/model.h
+++ Liberty/model/model.h
@@ -1,11 +1,29 @@
 #ifndef MODEL_H
 #define MODEL_H
 
+#include "veicolo.h"
+#include "container.h"
 
 class Model
 {
     public:
+        // Costruttori
         Model();
+        ~Model();
+
+        // Metodi costanti
+        const Veicolo* getVeicoloAt(const u_int&) const;
+        u_int count() const;
+        // get: Veicolo meno costoso perkm, Veicolo più costoso per km, get veicolo con Autonomia con un pieno maggiore
+
+        // Metodi non costanti
+        void add(Veicolo *);
+        void rifornisci(Veicolo *, Rifornimento *);
+        void delelte(Container<Veicolo *>::iterator &);
+        void deleteAll();
+
+    private:
+        Container<Veicolo*> veicoli;
 };
 
 #endif // MODEL_H
